741e39fa4e3e30aa943728cb6dbb8fc73061ed33
Corey Kirkland <82225343+coreykirkland@users.noreply.github.com>
2021-08-26 15:02:58 +0100

    Add files via upload

--- PercentKO.R
+++ PercentKO.R
@@ -14,7 +14,7 @@ library(dplyr)
 
 PercentTPM <- read.delim(PercentTPM, sep = "\t", header = TRUE)
 
-KO <- read.delim(KO, header = FALSE, sep = "\t")
+KO <- read.delim(KO, header = FALSE, sep = "\t", col.names = c("Contig", "GeneID", "KO"))
 # Paste columns 1 and 2 back together:
 KO <- data.frame(ClusterRep = paste(KO$Contig, KO$GeneID, sep = "_"), KO = KO$KO)
 
@@ -23,7 +23,7 @@ PercentTPM1 <- separate(PercentTPM, ClusterRep_MG, into=c("a","b","c","d","e","f
 PercentTPM1 <- data.frame(MG = PercentTPM1$MG, ClusterRep = paste(PercentTPM1$a, PercentTPM1$b, PercentTPM1$c, PercentTPM1$d, PercentTPM1$e, PercentTPM1$f, sep = "_"), ClusterRepMG = PercentTPM1$g, Cluster_Member = PercentTPM1$Cluster_Member, MG_GeneID = PercentTPM1$MG_GeneID, TPM = PercentTPM1$TPM, TotalTPM = PercentTPM1$TotalTPM, GFPercent = PercentTPM1$Percent)
 
 # Merge PercentTPM and KO:
-MergedDF <- merge(PercentTPM, KO, by = "ClusterRep", all = TRUE)
+MergedDF <- merge(PercentTPM1, KO, by = "ClusterRep", all = TRUE)
 MergedDF <- unique(MergedDF)
 MergedDF1 <- na.omit(MergedDF)
 MergedDF1 <- data.frame(MergedDF1, MG_KO = paste(MergedDF1$MG, MergedDF1$KO, sep = "_"))
