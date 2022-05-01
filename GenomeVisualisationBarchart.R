c41dd71255b9fd816da1dc9c8c0f936913c27de0
Corey Kirkland <82225343+coreykirkland@users.noreply.github.com>
2022-05-01 14:26:32 +0100

    Add files via upload

--- GenomeVisualisationBarchart.R
+++ GenomeVisualisationBarchart.R
@@ -6,8 +6,7 @@
 Args = commandArgs(TRUE)
 MergedKOAll <- Args[1]
 BarchartKO <- Args[2]
-BarchartFill <- Args[3]
-Output <- Args[4]
+Output <- Args[3]
 
 # Load Libraries:
 library(tidyr)
@@ -17,13 +16,21 @@ library(ggplot2)
 ## Visualisation of Data:
 
 # Read Data:
-MergedKOAll <- read.delim(MergedKOAll, header = TRUE, sep = "\t")
+MergedKOAll <- read.delim(MergedKOAll, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+#MergedKOAll[is.na(MergedKOAll)] = "Unbinned"
 
 # Barchart - MergedKOAll: 
 BarchartKO <- data.frame(BarchartKO)
 BarchartDF <- filter(MergedKOAll, KO %in% BarchartKO$BarchartKO)
+BarchartDF <- na.omit(BarchartDF)
 write.table(BarchartDF, file = paste(Output, 'BarchartKO.tsv', sep = ""), sep = "\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
 
+
+##### For another taxonomic level change BarchartDF$Order to BarchartDF$Phylum for example #####
+BarchartDF <- data.frame(KO = BarchartDF$KO, MG = BarchartDF$MG, Taxon = BarchartDF$Order, KOPercent = BarchartDF$KOPercent) 
+#####
+
+
 pdf(file = paste(Output, 'BarchartKO.pdf', sep = ""), paper = "a4")
-ggplot(BarchartDF, aes(fill = get(BarchartFill), y=KOPercent, x=MG)) + geom_bar(position='stack', stat='identity', colour = "white", ) + xlab("Metagenome") + ylab("%") + theme(axis.text.x = element_text(angle = 90))
-dev.off() 
+ggplot(BarchartDF, aes(fill = Taxon, x = MG)) + geom_bar(colour = "white", size = 0.01) + xlab("Metagenome") + ylab("% of mapped reads") + theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1)) + labs(fill='Taxon') +scale_y_continuous(limits=c(0,100))
+dev.off()
--- metaMAG_visualisation_barchart.sh
+++ metaMAG_visualisation_barchart.sh
@@ -3,22 +3,25 @@
 	set -u
 	set -o pipefail
 
-while getopts k:b:o: flag
+while getopts k:f:o: flag
 do
 	case "${flag}" in
 		k) BARCHARTKO=${OPTARG};;
-		b) BARCHARTFILL=${OPTARG};;
+		f) FILENAME=${OPTARG};;
 		o) OUTPUT=${OPTARG};;
 	esac
 done
 
 echo "metaMAG: $0"
 echo "Barchat KO: $BARCHARTKO"; 
-echo "Barchart Taxon: $BARCHARTFILL";
+echo "File Name Prefix: $FILENAME";
 echo "metaMAG Output Directory: $OUTPUT"
 
+#test -d some_directory ; echo $? # is this a directory? 
+#test -f some_file.txt ; echo $? # is this a file?
+#test -r some_file.txt ; echo $? $ is this file readable?
 
-echo "###### metaMAG_visualisation_barchart module: Updated 30/04/22 ######"
+echo "###### metaMAG_visualisation_barchart module: Updated 01/05/22 ######"
 
 # Variables:
 metaMAG="$OUTPUT"/metaMAG/
@@ -26,11 +29,9 @@ metaMAG="$OUTPUT"/metaMAG/
 ###################################
 
 # Rscript output:
-ROutput="$metaMAG"/metaMAG/Genome_Visualisation/"$BARCHARTKO"_"$BARCHARTFILL"_
+ROutput="$metaMAG"/metaMAG/Genome_Visualisation/"$FILENAME"_
 
 # Rscript:
-Rscript GenomeVisualisationBarchart.R "$metaMAG"/metaMAG/Genome_Visualisation/MergedKOAll.tsv "$BARCHARTKO" "$BARCHARTFILL" "$ROutput"
+Rscript GenomeVisualisationBarchart.R "$metaMAG"/metaMAG/Genome_Visualisation/MergedKOAll.tsv "$BARCHARTKO" "$ROutput"
 
 # Outputs: BarchartKO.tsv BarchartKO.tsv
-
-echo "### END OF MODULE ###"
